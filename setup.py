aebbe77ab121978633b6ee7df6ab4f37334d86d7
Jakub Matys <jakub.matys@nodus.cz>
2013-06-20 10:41:17 +0200

    Removed brackets around desc in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='nas-power-manager',
       version='0.1',
-      description=('Power manager for NAS systems.'),
+      description='Power manager for NAS systems.',
       author='Jakub Matys',
       author_email='matys.jakub@gmail.com',
       url='https://github.com/jakm/nas-power-manager',
